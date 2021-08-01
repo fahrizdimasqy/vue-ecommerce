128497681d98cc78a79745969f00ba060f0e800b
fahrizdimasqy <1197050037@student.uinsgd.ac.id>
2021-08-01 21:47:41 +0700

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,75 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
+  <v-app>
+    <v-app-bar app color="white" light>
+      <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
+      <v-spacer></v-spacer>
+      <v-btn icon @click="search = !search">
+        <v-icon>mdi-magnify</v-icon>
+      </v-btn>
+      <v-text-field
+        slot="extension"
+        hide-details
+        append-icon="mdi-microphone"
+        flat
+        label="Search"
+        prepend-inner-icon="mdi-magnify"
+        solo-inverted
+        v-show="search"
+      ></v-text-field>
+      <v-btn icon>
+        <v-badge color="orange" overlap>
+          <template v-slot:badge>
+            <span>3</span>
+          </template>
+          <v-icon>mdi-cart</v-icon>
+        </v-badge>
+      </v-btn>
+    </v-app-bar>
 
-<style lang="stylus">
-#app
-  font-family Avenir, Helvetica, Arial, sans-serif
-  -webkit-font-smoothing antialiased
-  -moz-osx-font-smoothing grayscale
-  text-align center
-  color #2c3e50
-  margin-top 60px
+    <v-navigation-drawer app v-model="drawer"> </v-navigation-drawer>
+
+    <v-content>
+      <v-container fluid>
+        <v-slide-y-transition mode="out-in">
+          <router-view></router-view>
+        </v-slide-y-transition>
+      </v-container>
+    </v-content>
+    <v-bottom-navigation :value="value" color="primary">
+      <v-btn>
+        <span>Recents</span>
+
+        <v-icon>mdi-history</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Favorites</span>
+
+        <v-icon>mdi-heart</v-icon>
+      </v-btn>
+
+      <v-btn>
+        <span>Nearby</span>
+
+        <v-icon>mdi-map-marker</v-icon>
+      </v-btn>
+    </v-bottom-navigation>
+  </v-app>
+</template>
+<style scoped>
+* {
+  font-family: "Nunito";
+}
+.v-sheet.v-app-bar.v-toolbar:not(.v-sheet--outlined) {
+  box-shadow: none;
+}
 </style>
+<script>
+export default {
+  name: "App",
+  data: () => ({
+    drawer: false,
+    search: false,
+  }),
+};
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,95 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div>
+    <h3>Hello Fahriz Dimasqy</h3>
+    <p class="text--disabled">Let's gets something</p>
+    <v-sheet class="mx-auto" elevation="8" max-width="800">
+      <v-slide-group
+        v-model="model"
+        class="pa-4"
+        active-class="success"
+        show-arrows
+      >
+        <v-slide-item v-for="n in 15" :key="n" v-slot="{ active, toggle }">
+          <v-card
+            :color="active ? undefined : 'grey lighten-1'"
+            class="ma-4"
+            height="200"
+            width="400"
+            @click="toggle"
+          >
+            <v-row class="fill-height" align="center" justify="center">
+              <v-scale-transition>
+                <v-icon
+                  v-if="active"
+                  color="white"
+                  size="48"
+                  v-text="'mdi-close-circle-outline'"
+                ></v-icon>
+              </v-scale-transition>
+            </v-row>
+          </v-card>
+        </v-slide-item>
+      </v-slide-group>
+    </v-sheet>
+
+    <v-sheet class="" max-width="700">
+      <v-slide-group multiple show-arrows>
+        <v-slide-item v-for="n in 25" :key="n" v-slot="{ active, toggle }">
+          <v-btn
+            class=""
+            :input-value="active"
+            active-class="purple white--text"
+            depressed
+            rounded
+            @click="toggle"
+          >
+            Options {{ n }}
+          </v-btn>
+        </v-slide-item>
+      </v-slide-group>
+    </v-sheet>
+
+    <v-row dense class="mt-2">
+      <v-col v-for="card in cards" :key="card.title" :cols="card.flex">
+        <v-card>
+          <v-img
+            :src="card.src"
+            class="white--text align-end"
+            gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
+            height="200px"
+          >
+          </v-img>
+          <v-card-title v-text="card.title"></v-card-title>
+          <v-card-text v-text="card.price"></v-card-text>
+        </v-card>
+      </v-col>
+    </v-row>
   </div>
 </template>
-
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
-
-export default {
-  name: 'Home',
-  components: {
-    HelloWorld
-  }
+<style scoped>
+.v-application .elevation-8 {
+  box-shadow: none !important;
 }
+</style>
+<script>
+export default {
+  name: "Home",
+  data: () => ({
+    cards: [
+      {
+        title: "Apple Watch",
+        price: "Rp. 1.50000",
+        src: "https://cdn.vuetifyjs.com/images/cards/road.jpg",
+        flex: 6,
+      },
+      {
+        title: "Best airlines",
+        price: "Rp. 1.50000",
+        src: "https://cdn.vuetifyjs.com/images/cards/plane.jpg",
+        flex: 6,
+      },
+    ],
+    model: null,
+  }),
+};
 </script>
